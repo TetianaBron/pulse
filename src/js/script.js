06a140ae01b2153f9cfc10b61f301601b302b4d8
Tetiana Bronitska <tanya_nest@ukr.net>
2021-09-01 02:09:15 +0300

    Added catalog

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-//script for slick-slider
+//script from jQuery for slick-slider
 // $(document).ready(function () {
 //   $('.carousel__inner').slick({
 //     speed: 1200,
@@ -14,7 +14,6 @@
 //             }
 //         },
 //     ]
-//     });
 // });
 
 // var slider = tns({
@@ -32,8 +31,6 @@ const slider = tns({
     controls: false,
     responsive: {
         320: {
-            // edgePadding: 20,
-            // gutter: 20,
             nav: true
         },
         991: {
@@ -49,3 +46,38 @@ document.querySelector('.prev').addEventListener('click', function () {
 document.querySelector('.next').addEventListener('click', function () {
     slider.goTo('next');
 });
+
+// Script from jQuery for tabs
+// $(document).ready(function () {
+//     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+//     $(this)
+//       .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+//       .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+//   });
+// });
+
+(function($) {
+$(function() {
+  
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+    
+    function toggleSlide(item) {
+        $(item).each(function (i) {
+        $(this).on('click', function (e) {
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+
+        })
+        })
+    }
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+  
+});
+})(jQuery);
