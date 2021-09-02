272b5a812babc37138374372c29ad64875bd08c5
Tetiana Bronitska <tanya_nest@ukr.net>
2021-09-02 22:39:38 +0300

    Added validation for forms

--- src/js/script.js
+++ src/js/script.js
@@ -99,5 +99,37 @@ $(function() {
         })
     });
 
+    //Validation for forms
+    function validateForms (form) {
+        $(form).validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2
+            },
+            phone: "required",
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            name: {
+                required: "Пожалуйста, введите свое имя",
+                minlength: jQuery.validator.format("Введите минимум {0} буквы")
+                },
+            phone: "Пожалуйста, введите свой номер телефона",
+            email: {
+                required: "Пожалуйста, введите свою почту",
+                email: "Неправильно введен адрес почты"
+            }           
+        }
+    });
+    };
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
 });
 })(jQuery);
