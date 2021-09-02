8817491c2acf1607d9d5e34c9be516ffb2cfc27c
Tetiana Bronitska <tanya_nest@ukr.net>
2021-09-02 03:18:23 +0300

    Added modals

--- src/js/script.js
+++ src/js/script.js
@@ -23,6 +23,7 @@
 //     autoplay: true
 //   });
 
+//Script for slider - Vanilla JS
 const slider = tns({
     container: '.carousel__inner',
     items: 1,
@@ -47,7 +48,7 @@ document.querySelector('.next').addEventListener('click', function () {
     slider.goTo('next');
 });
 
-// Script from jQuery for tabs
+// Ogigin script from jQuery for tabs
 // $(document).ready(function () {
 //     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
 //     $(this)
@@ -56,7 +57,9 @@ document.querySelector('.next').addEventListener('click', function () {
 //   });
 // });
 
-(function($) {
+//jQuery
+(function ($) {
+// My script from jQuery for tabs
 $(function() {
   
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
@@ -65,6 +68,7 @@ $(function() {
         .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
     
+    // Script for toogle "Details" to "Back" in cards of catalog
     function toggleSlide(item) {
         $(item).each(function (i) {
         $(this).on('click', function (e) {
@@ -78,6 +82,22 @@ $(function() {
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
-  
+    
+
+    //Script for Modal
+    $('[data-modal=consultation]').on('click', function () {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function () {
+        $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+    });
+    
+    $('.button_mini').each(function (i) {
+        $(this).on('click', function () {
+            $('#order .modal__text').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        })
+    });
+
 });
 })(jQuery);
