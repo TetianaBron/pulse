c09790a2bffb498e00a3aab931729c7ce48b3e6c
Tetiana Bronitska <tanya_nest@ukr.net>
2021-09-07 04:40:19 +0300

    Added files for local server

--- src/js/script.js
+++ src/js/script.js
@@ -133,5 +133,22 @@ $(function() {
 
     // Mask for phone in inputs
     $('input[name="phone"]').mask("+7 (999) 999-99-99");
+
+    //For sending data from forms to email
+    $('form').on("submit", function (e) {
+        e.preventDefault;
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function () {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
 });
 })(jQuery);
